3a2c272104d90c8ff87719ad815e3677879619a0
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-06-12 20:47:49 -0500

    [+] app.routes.ts: Define rutas [+] pages/: Define componentes (public, private) [+] product.component: Define Formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/public/home/home';
+import { Login } from './pages/public/login/login';
+import { Register } from './pages/public/register/register';
+import { Products } from './pages/private/products/products';
+import { ProductNewForm } from './pages/private/products/new-form/new-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'login', component: Login },
+    { path: 'register', component: Register },
+    { path: 'dashboard/products', component: Products },
+    { path: 'dashboard/products/new', component: ProductNewForm  },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home', pathMatch: "full" }
+];
