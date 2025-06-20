ae11497bb68e80bb2866812124c3cf22934fcd8e
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-06-19 21:47:26 -0500

    [+] app.config.ts: Configura provideHttpClient [+] ProductNewForm: Define campos y validaciones del formulario de registrar producto

--- src/app/pages/private/products/new-form/new-form.ts
+++ src/app/pages/private/products/new-form/new-form.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CategoryService } from '../../../../services/category-service';
 
 @Component({
   selector: 'app-new-form',
@@ -9,20 +10,51 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ProductNewForm {
   formData!: FormGroup;  
+  categories: any = [];
 
-  constructor() {
+  constructor( private categoryService: CategoryService ) {
     this.formData = new FormGroup({
-      name: new FormControl(),
-      description: new FormControl(),
-      price: new FormControl(),
-      stock: new FormControl(),
+      name: new FormControl( '', [ Validators.required, Validators.minLength( 5 ), Validators.maxLength( 50 ) ] ),
+      description: new FormControl( '', [] ),
+      price: new FormControl( 0, [ Validators.required, Validators.min( 0 ) ] ),
+      stock: new FormControl( 1, [ Validators.required, Validators.min( 1 ), Validators.max( 100 ) ] ),
       urlImage: new FormControl(),
-      category: new FormControl(),
-      state: new FormControl()
+      category: new FormControl(),  // TODO: Traer los datos antes de establecer las reglas
+      state: new FormControl( true, [ Validators.required ] )
     });
   }
 
   onSubmit() {
-    console.log( this.formData.value );
+    console.log(
+      this.formData.valid,
+      this.formData.invalid,
+      this.formData.pristine,
+      this.formData.dirty,
+      this.formData.touched
+    );
+
+    if( this.formData.valid ) {
+      console.log( this.formData.value );
+    }
+
+    this.formData.reset();  // Limpiamos los campos del formulario
+  }
+
+  ngOnInit() {
+    this.categoryService.getCategories().subscribe({
+      next: ( data ) => {
+        console.log( data );
+        this.categories = data;
+      },
+      error: ( error ) => {
+        console.error( error );
+      },
+      complete: () => {
+        console.log( 'complete' );
+      }
+    });
+  }
+  ngOnDestroy() {
+    console.log( 'ngOnDestroy' );
   }
 }
